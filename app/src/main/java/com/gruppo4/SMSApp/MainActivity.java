95cc9aa6bd95527a97ece779a81cf0700002ab5b
francescobau <francescobau@users.noreply.github.com>
2019-10-27 21:25:20 +0100

    Updated my branch into master version 2.0
    Imported the version 2.0 from master trunk.
    Reason: learning.

--- app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
+++ app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
@@ -1,139 +1,150 @@
 package com.gruppo4.SMSApp;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.gruppo4.sms.SMSController;
 import com.gruppo4.sms.SMSMessage;
-import com.gruppo4.sms.interfaces.SMSReceivedListener;
-import com.gruppo4.sms.interfaces.SMSSentListener;
+import com.gruppo4.sms.SMSReceivedMessage;
+import com.gruppo4.sms.exceptions.InvalidSMSMessageException;
+import com.gruppo4.sms.exceptions.InvalidTelephoneNumberException;
+import com.gruppo4.sms.listeners.SMSReceivedListener;
+import com.gruppo4.sms.listeners.SMSSentListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SMSReceivedListener, SMSSentListener {
 
-    /**
-     * Il messaggio vero e proprio
-     */
-    SMSMessage smsMessage;
-    /**
-     * Le 3 caselle di testo, di cui la prima di queste e' quella in cui si scrive
-     * il numero di telefono a cui si vuole inviare lo smiley
-     */
-    TextView numberTextView, smileReceiver, textView;
-    /**
-     * Il bottone che serve per inviare lo smiley :)
-     */
-    Button smileButton;
+    private RecyclerView recyclerView;
+    private SmileAdapter mAdapter;
 
-    /**
-     * onCreate. la creazione dell'Activity. Fa parte del ciclo di vita dell'Activity.
-     */
+    private static final String SMILE_COMMAND = "SMILE_COMMAND";
+    private static final int SMILE_MESSAGE_ID = 1;
+    private static final int EXTRA_MESSAGE_ID = 543;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-    /**
-     * Operazioni preliminari, realizzate nella superclasse.
-     */
         super.onCreate(savedInstanceState);
-    /**
-     * setContentView serve a collegare i nostri oggetti a un certo layout (?),
-     * in questo caso si tratta di activity_main nel package layout di res (R).
-     */
         setContentView(R.layout.activity_main);
 
-     /**
-      * R sta per Resources.
-      * R e' molto utile per il collegamento diretto alle Risorse (package "res").
-      * findViewById serve per cercare la risorsa, e abbinarla alla nostra
-      * variabile d'esemplare.
-      * Si ha creato delle variabili d'esemplare, e adesso si vuole collegarle
-      * al loro identificativo, in questo caso e' relativo all'activity_main in layout.
-      */
-        numberTextView = findViewById(R.id.number);
-        smileButton = findViewById(R.id.smile_button);
-        smileReceiver = findViewById(R.id.smileReceiver);
-        textView = findViewById(R.id.textView);
+        //Permission to receive messages
+        requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, 1);
+        //Permission to send messages
+        requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
 
-      /**
-       * Questa Activity viene passata come listener (???)
-       */
-        SMSController.addOnReceivedListener(this);
+        ArrayList<String> smiles = new ArrayList<>();
 
-        /**
-         * Si imposta l'azione collegata al clic del bottone smileButton.
-         */
-        smileButton.setOnClickListener(new View.OnClickListener() {
+        recyclerView = findViewById(R.id.my_recycler_view);
+
+        // use a linear layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new SmileAdapter(smiles);
+        recyclerView.setAdapter(mAdapter);
+
+        SMSController.setup(this, 123);
+
+        SMSController.addOnReceiveListener(this, SMILE_MESSAGE_ID);
+
+        findViewById(R.id.sendSmileButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-      /**
-       * Si estrae il numero telefonico dalla casella di testo.
-       * Dato che l'output e' di tipo CharSequence, allora lo si deve
-       * trasformare in stringa, grazie al metodo toString().
-       */
-                String telephoneNumber = numberTextView.getText().toString();
-       /**
-        * Controllo per verificare se il numero di telefono e' valido o no.
-        * Credits: Luca Crema feat. gruppo4.
-       */
-                if (!SMSMessage.checkTelephoneNumber(telephoneNumber)) {
-       /**
-        * Il Toast e' fantastico.
-        * Questo Toast serve a creare una piccola finestra a comparsa in basso
-        * che notifica se il numero inserito e' corretto oppure no.
-        * Il .show() finale serve per svelare il Toast appena creato.
-        * Appena fatto un toast, lo si vuole servire a tavola! :-P
-       */
-                    Toast.makeText(MainActivity.this, "Wrong telephone number", Toast.LENGTH_LONG).show();
-                    return;
-                }
-       /**
-        * Il messaggio vero e proprio! :)
-        * Si passa per parametro il numero di telefono, e la stringa testuale!
-        */
-                smsMessage = new SMSMessage(telephoneNumber, "Sent you a smile :)");
-        /**
-         * L'invio del messaggio, gestito da SMSController, che a sua volta
-         * sfrutta in parte la classe SMSManager della libreria Android.
-         */
-                SMSController.sendMessage(smsMessage, getBaseContext(), MainActivity.this);
+                onSendSmileButton();
+            }
+        });
+        findViewById(R.id.sendExtraCode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSendWrongCodeButton();
             }
         });
     }
 
     /**
-     * onSentReceived. Realizzazione del metodo nell'interfaccia SMSSentListener,
-     * che si occupa di verificare se il messaggio e' stato inviato o no.
-     * Credits: Luca Crema feat. gruppo4
+     * Sends a message using the sms library
+     *
+     * @param text
+     * @param telephoneNumber
+     * @param messageCode
      */
-    @Override
-    public void onSentReceived(SMSMessage message) {
-    /**
-     * Si crea un Toast che informa sullo stato del messaggio (inviato/non inviato).
-     */
-        switch (message.getState()) {
-            case MESSAGE_SENT:
-                Toast.makeText(this, "Message sent", Toast.LENGTH_SHORT).show();
-                textView.setText("Message sent to " + smsMessage.getTelephoneNumber());
-                break;
-            default:
-                Toast.makeText(this, "Message not sent", Toast.LENGTH_SHORT).show();
-                textView.setText("Message not sent to " + smsMessage.getTelephoneNumber());
-                break;
+    private void sendMessage(String text, String telephoneNumber, int messageCode) {
+        try {
+            SMSMessage message = new SMSMessage(telephoneNumber, text, messageCode);
+            SMSController.sendMessage(message, this);
+        } catch (InvalidSMSMessageException messageException) {
+            Log.e("MainActivity", messageException.getMessage());
+        } catch (InvalidTelephoneNumberException telephoneException) {
+            String error = "";
+            switch (telephoneException.getState()) {
+                case TELEPHONE_NUMBER_TOO_SHORT:
+                    error = "Telephone number is too short!";
+                    break;
+                case TELEPHONE_NUMBER_TOO_LONG:
+                    error = "Telephone number is too long!";
+                    break;
+                case TELEPHONE_NUMBER_NO_COUNTRY_CODE:
+                    error = "You have to insert the country code";
+                    break;
+                case TELEPHONE_NUMBER_NOT_A_NUMBER:
+                    error = "The telephone number is not valid";
+                    break;
+            }
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+            Log.e("MainActivity", telephoneException.getMessage());
         }
+    }
 
+    public void onSendWrongCodeButton() {
+        String phoneNumber = ((AutoCompleteTextView) findViewById(R.id.phoneNumberTextView)).getText().toString();
+        sendMessage(SMILE_COMMAND, phoneNumber, EXTRA_MESSAGE_ID);
     }
 
     /**
-     * onMessageReceived. Realizzazione del metodo nell'interfaccia SMSReceivedListener,
-     * che si occupa della ricezione del messaggio.
-     * Credits: Luca Crema feat. gruppo4.
+     * Callback for send smile button pressed. Sends a message to the number specified in the phoneNumberTextView
      */
+    public void onSendSmileButton() {
+        String phoneNumber = ((AutoCompleteTextView) findViewById(R.id.phoneNumberTextView)).getText().toString();
+        sendMessage(SMILE_COMMAND, phoneNumber, SMILE_MESSAGE_ID);
+    }
+
     @Override
-    public void onMessageReceived(SMSMessage message) {
-        smileReceiver.setText(message.getTelephoneNumber() + " " + message.getMessage());
+    public void onSMSReceived(SMSReceivedMessage message) {
+        if (message.getMessage().equals(SMILE_COMMAND)) {
+            Log.d("MainActivity", "Received message:" + message.getMessage());
+            Toast.makeText(this, message.getTelephoneNumber() + " sent you a smile :)", Toast.LENGTH_LONG).show();
+            mAdapter.getSmiles().add(message.getTelephoneNumber() + " sent you a smile :)");
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onSMSSent(SMSMessage message, SMSMessage.SentState state) {
+        Log.d("MainActivity", "Message sent");
+        if (state == SMSMessage.SentState.MESSAGE_SENT) {
+            if (message.getMessageCode() == SMILE_MESSAGE_ID) {
+                Toast.makeText(this, "Smile sent :)", Toast.LENGTH_SHORT).show();
+                mAdapter.getSmiles().add("You sent a smile to " + message.getTelephoneNumber());
+                mAdapter.notifyDataSetChanged();
+            } else {
+                Toast.makeText(this, "Message sent", Toast.LENGTH_SHORT).show();
+                mAdapter.getSmiles().add("You sent a message to " + message.getTelephoneNumber());
+                mAdapter.notifyDataSetChanged();
+            }
+        } else {
+            Log.w("MainActivity", "Unable to send sms, reason: " + state);
+            if (message.getMessageCode() == SMILE_MESSAGE_ID)
+                Toast.makeText(this, "Unable to send smile :(", Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(this, "Unable to send message, reason: " + state, Toast.LENGTH_LONG).show();
+        }
     }
 }
