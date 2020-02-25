243def26aa96c46fa7020e157f001fcd4e826b56
francescobau <francescobau@users.noreply.github.com>
2020-02-25 11:39:00 +0100

    Added UI basic objects, updated strings.xml and added View objects in MainActivity, updated minSDK to 23 instead of 26, fixed AndroidManifest.xml, restored needed res files, added onClickListener for sendButton.
    This is a basic project, even lower SDK versions can handle this.

--- app/src/main/java/com/gruppo4/ringUp/activities/MainActivity.java
+++ app/src/main/java/com/gruppo4/ringUp/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.gruppo4.ringUp.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +11,19 @@ import com.gruppo4.ringUp.R;
 
 /**
  * @author Francesco Bau'
+ * @version 0.1
+ * <p>
+ * Main Activity. It takes UI commands as input, and, as an output, it gives them a certain action.
+ * Its current scope is just to send SMS, based by what does the User decide to do.
  * @since 24/02/2020
  */
 public class MainActivity extends AppCompatActivity {
+
+    View ringButton;
+    View locateButton;
+    View lockButton;
+
+    View sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +32,35 @@ public class MainActivity extends AppCompatActivity {
 
         Context context = getApplicationContext();
 
+        String telephoneNumber = findViewById(R.id.telephoneNumber).toString();
+        Log.d("TelephoneNumberCheck", "Telephone number: " + telephoneNumber);
 
+
+        ringButton = findViewById(R.id.ringButton);
+        locateButton = findViewById(R.id.locateButton);
+        lockButton = findViewById(R.id.lockButton);
+
+        sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(this::onSend);
+
+
+    }
+
+    /**
+     * Listener activated when user presses the "SEND" button.
+     * It performs the action, based by the RadioButton choice.
+     *
+     * @param view The current view.
+     * @throws IllegalArgumentException if nothing is selected in the RadioButton.
+     */
+    public void onSend(View view) throws IllegalArgumentException {
+        if (ringButton.isSelected())
+            Log.d("RingButton", "RING button is selected.");
+        else if (locateButton.isSelected())
+            Log.d("LocateButton", "Locate button is selected.");
+        else if (lockButton.isSelected())
+            Log.d("LockButton", "LOCK button is selected.");
+        else throw new IllegalArgumentException("Nothing is selected.");
     }
 
 
