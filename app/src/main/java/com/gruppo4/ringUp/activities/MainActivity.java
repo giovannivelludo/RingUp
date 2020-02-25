a06fc663c1e1f8990ae127933e9e6c598081000d
francescobau <francescobau@users.noreply.github.com>
2020-02-25 12:42:03 +0100

    Added enum SelectedCommand
    KNOWN ISSUES:
    - management of the INVALID or null command
    - get text from telephoneNumber fiels

--- app/src/main/java/com/gruppo4/ringUp/activities/MainActivity.java
+++ app/src/main/java/com/gruppo4/ringUp/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.gruppo4.ringUp.R;
@@ -19,11 +20,17 @@ import com.gruppo4.ringUp.R;
  */
 public class MainActivity extends AppCompatActivity {
 
-    View ringButton;
-    View locateButton;
-    View lockButton;
+    String telephoneNumber;
+    View ringButton, locateButton, lockButton;
 
     View sendButton;
+
+    enum SelectedCommand {
+        INVALID, RING, LOCATE, LOCK;
+    }
+
+    SelectedCommand command;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,35 +39,69 @@ public class MainActivity extends AppCompatActivity {
 
         Context context = getApplicationContext();
 
-        String telephoneNumber = findViewById(R.id.telephoneNumber).toString();
+        telephoneNumber = findViewById(R.id.telephoneNumber).toString();
         Log.d("TelephoneNumberCheck", "Telephone number: " + telephoneNumber);
 
 
         ringButton = findViewById(R.id.ringButton);
+        ringButton.setOnClickListener(this::onClick);
         locateButton = findViewById(R.id.locateButton);
+        locateButton.setOnClickListener(this::onClick);
         lockButton = findViewById(R.id.lockButton);
+        lockButton.setOnClickListener(this::onClick);
 
         sendButton = findViewById(R.id.sendButton);
         sendButton.setOnClickListener(this::onSend);
 
+        Log.d("COMMAND-TEST", "Testing command: " + command);
 
     }
+
 
     /**
      * Listener activated when user presses the "SEND" button.
      * It performs the action, based by the RadioButton choice.
      *
-     * @param view The current view.
-     * @throws IllegalArgumentException if nothing is selected in the RadioButton.
+     * @param view The current view. SEND button is expected. It can't be null.
+     * @throws IllegalArgumentException if nothing is selected in the RadioButton, or
+     *                                  if an unexpected command is selected.
      */
-    public void onSend(View view) throws IllegalArgumentException {
-        if (ringButton.isSelected())
-            Log.d("RingButton", "RING button is selected.");
-        else if (locateButton.isSelected())
-            Log.d("LocateButton", "Locate button is selected.");
-        else if (lockButton.isSelected())
-            Log.d("LockButton", "LOCK button is selected.");
+    private void onSend(@NonNull View view) throws IllegalArgumentException {
+        if (command == SelectedCommand.RING)
+            Log.d("BUTTON-CHECKED", "RING button is selected.");
+        else if (command == SelectedCommand.LOCATE)
+            Log.d("BUTTON-CHECKED", "Locate button is selected.");
+        else if (command == SelectedCommand.LOCK)
+            Log.d("BUTTON-CHECKED", "LOCK button is selected.");
+        else if (command == SelectedCommand.INVALID)
+            throw new IllegalArgumentException("Unexpected command is selected");
         else throw new IllegalArgumentException("Nothing is selected.");
+    }
+
+    /**
+     * Listener activated when a choice is made by the user.
+     * The String modifies based by the user's choice.
+     *
+     * @param view The current view. It can't be null.
+     */
+    private void onClick(@NonNull View view) {
+        switch (view.getId()) {
+            case R.id.ringButton: {
+                command = SelectedCommand.RING;
+            }
+            break;
+            case R.id.locateButton: {
+                command = SelectedCommand.LOCATE;
+            }
+            break;
+            case R.id.lockButton: {
+                command = SelectedCommand.LOCK;
+            }
+            break;
+            default:
+                command = SelectedCommand.INVALID;
+        }
+        Log.d("SELECTED-COMMAND","Selected command: "+command);
     }
 
 
